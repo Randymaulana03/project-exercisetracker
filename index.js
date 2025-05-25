98f992b3b541ab3c5d1b0e1adcf25823fd57fd03
Randy Maulana <randy8@mhs.usk.ac.id>
2025-05-26 00:47:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const dns = require('node:dns');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const arrayOfURLs = ["Wrong Format"];
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path, req.ip)
+  next();
+})
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+// Middleware for parsing req.body 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Your first API endpoint
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+app.post('/api/shorturl', (req, res) => {
+  let checkLink = req.body.url;
+  const url = new URL(req.body.url).hostname;
+  dns.lookup(url, (err) => {
+    if (err) res.json({ "error": "Invalid URL" })
+    else {
+      arrayOfURLs.indexOf(checkLink) == -1 ? arrayOfURLs.push(checkLink) : null;
+      res.json({ "original_url": req.body.url, "short_url": arrayOfURLs.indexOf(checkLink) })
+    }
+  })
 })
+
+app.get('/api/shorturl/:index', (req, res) => {
+  if (req.params.index == 0) {
+    res.json({ "error": "Wrong format" });
+  }
+  else if (req.params.index == undefined) {
+    res.json({ "error": "Invalid number" })
+  }
+  else {
+    res.status(301).redirect(arrayOfURLs[req.params.index]);
+  }
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
